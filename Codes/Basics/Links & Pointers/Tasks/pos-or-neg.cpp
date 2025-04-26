08356a303543e42f8d96c6c3c4d6acdce29eeb57
NaLavte <matvejportnagin61@gmail.com>
2025-04-26 17:33:05 +0500

    Solved task

---
+++ Codes/Basics/Links & Pointers/Tasks/pos-or-neg.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RU");
+
+	int a;
+	cout << "Введите любой число: ";
+	cin >> a;
+	int* ptrA = &a;
+	cout << (*ptrA < 0 ? "Число отрецательное" : "Число положительное");
+}
